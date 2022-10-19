58c5be9775fafc65c2719938ce2f5fdb618a7e06
Henrik Panhans <henrik@panhans.dev>
2022-10-19 10:57:08 +0200

    Extracting lots of code out of main.py
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- main.py
+++ main.py
@@ -8,71 +8,36 @@ This is:
 """
 import logging
 import signal
-import random
+import bme680
 
-from pyhap.accessory import Accessory
+from pyhap.accessory import Bridge
 from pyhap.accessory_driver import AccessoryDriver
-from pyhap.const import CATEGORY_SENSOR
+from temperature_sensor import TemperatureSensor
 
 logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
 
-
-class TemperatureSensor(Accessory):
-    """Implementation of a mock temperature sensor accessory."""
-
-    category = CATEGORY_SENSOR  # This is for the icon in the iOS Home app.
-
-    def __init__(self, *args, **kwargs):
-        """Here, we just store a reference to the current temperature characteristic and
-        add a method that will be executed every time its value changes.
-        """
-        # If overriding this method, be sure to call the super's implementation first.
-        super().__init__(*args, **kwargs)
-
-        # Add the services that this Accessory will support with add_preload_service here
-        temp_service = self.add_preload_service("TemperatureSensor")
-        self.temp_char = temp_service.get_characteristic("CurrentTemperature")
-
-        # Having a callback is optional, but you can use it to add functionality.
-        self.temp_char.setter_callback = self.temperature_changed
-
-    def temperature_changed(self, value):
-        """This will be called every time the value of the CurrentTemperature
-        is changed. Use setter_callbacks to react to user actions, e.g. setting the
-        lights On could fire some GPIO code to turn on a LED (see pyhap/accessories/LightBulb.py).
-        """
-        print("Temperature changed to: ", value)
-
-    @Accessory.run_at_interval(60)  # Run this method every 60 seconds
-    # The `run` method can be `async` as well
-    async def run(self):
-        """We override this method to implement what the accessory will do when it is
-        started.
-
-        We set the current temperature to a random number. The decorator runs this method
-        every 60 seconds.
-        """
-        current_temp = random.randint(14, 26)
-        self.temp_char.set_value(current_temp)
-
-    # The `stop` method can be `async` as well
-    async def stop(self):
-        """We override this method to clean up any resources or perform final actions, as
-        this is called by the AccessoryDriver when the Accessory is being stopped.
-        """
-        print("Stopping accessory.")
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except (RuntimeError, IOError):
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
 
-def get_accessory(driver):
-    """Call this method to get a standalone Accessory."""
-    return TemperatureSensor(driver, "MyTempSensor")
+def make_bridge(accessory_driver):
+    """Call this method to get a Bridge instead of a standalone accessory."""
+    bridge = Bridge(accessory_driver, 'RaspiBridge')
+    temp_sensor = TemperatureSensor(accessory_driver, 'TempSensor')
+    # temp_sensor2 = TemperatureSensor(accessory_driver, 'Sensor 1')
+    bridge.add_accessory(temp_sensor)
+    bridge.add_accessory(temp_sensor2)
+
+    return bridge
 
 
 # Start the accessory on port 51826
 driver = AccessoryDriver(port=51826)
 
 # Change `get_accessory` to `get_bridge` if you want to run a Bridge.
-driver.add_accessory(accessory=get_accessory(driver))
+driver.add_accessory(accessory=make_bridge(driver))
 
 # We want SIGTERM (terminate) to be handled by the driver itself,
 # so that it can gracefully stop the accessory, server and advertising.

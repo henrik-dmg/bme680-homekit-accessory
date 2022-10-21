9bba69bddd37be6c61bd4c825364300fb0e9d7c0
Henrik Panhans <henrik@panhans.dev>
2022-10-21 17:45:27 +0200

    Maybe now?
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ sensor.burn_in_sensor()
 def make_bridge(accessory_driver):
     bridge = Bridge(accessory_driver, 'RaspiBridge')
 
-    bridge.add_accessory(TemperatureSensor(accessory_driver, 'TemperatureSensor', sensor=sensor))
-    bridge.add_accessory(HumiditySensor(accessory_driver, 'HumiditySensor', sensor=sensor))
-    bridge.add_accessory(AirQualitySensor(accessory_driver, "AirQualitySensor", sensor=sensor))
+    bridge.add_accessory(TemperatureSensor(accessory_driver, sensor, 'TemperatureSensor'))
+    bridge.add_accessory(HumiditySensor(accessory_driver, sensor, 'HumiditySensor'))
+    bridge.add_accessory(AirQualitySensor(accessory_driver, sensor, "AirQualitySensor"))
 
     return bridge
 

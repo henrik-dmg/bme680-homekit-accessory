fb12881a74b3e02de2edab0b408d39310c87a1f8
Henrik Panhans <henrik@panhans.dev>
2022-10-21 18:05:31 +0200

    add callbacks
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- sensors/air_quality_sensor.py
+++ sensors/air_quality_sensor.py
@@ -13,6 +13,7 @@ class AirQualitySensor(Accessory):
         # Add the services that this Accessory will support with add_preload_service here
         aqi_service = self.add_preload_service("AirQualitySensor")
         self.aqi_char = aqi_service.get_characteristic("AirQuality")
+        self.aqi_char.setter_callback = self.aqi_changed
 
         # Keep reference of sensor
         self.sensor = kwargs.get("sensor")
@@ -21,3 +22,10 @@ class AirQualitySensor(Accessory):
     async def run(self):
         # 0 is the value for unknown air quality
         self.aqi_char.set_value(self.sensor.get_air_quality())
+
+    def aqi_changed(self, value):
+        """This will be called every time the value of the CurrentTemperature
+        is changed. Use setter_callbacks to react to user actions, e.g. setting the
+        lights On could fire some GPIO code to turn on a LED (see pyhap/accessories/LightBulb.py).
+        """
+        print('AQI changed to: ', value)
--- sensors/temperature_sensor.py
+++ sensors/temperature_sensor.py
@@ -14,10 +14,18 @@ class TemperatureSensor(Accessory):
         # Add the services that this Accessory will support with add_preload_service here
         temp_service = self.add_preload_service("TemperatureSensor")
         self.temp_char = temp_service.get_characteristic("CurrentTemperature")
-
+        self.temp_char.setter_callback = self.temperature_changed
+        
         # Keep reference of sensor
         self.sensor = kwargs.get("sensor")
 
     @Accessory.run_at_interval(60)
     async def run(self):
         self.temp_char.set_value(self.sensor.get_temperature())
+
+    def temperature_changed(self, value):
+        """This will be called every time the value of the CurrentTemperature
+        is changed. Use setter_callbacks to react to user actions, e.g. setting the
+        lights On could fire some GPIO code to turn on a LED (see pyhap/accessories/LightBulb.py).
+        """
+        print('Temperature changed to: ', value)
